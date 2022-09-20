ffc8c3470db143722210df137277da665db846be
ElPatatone <rahman.hm2002@gmail.com>
2022-09-21 00:55:44 +0100

    fix: add requirement.txt for deployment

--- app.py
+++ app.py
@@ -1,11 +1,6 @@
 import streamlit as st
 import pandas as pd
-import numpy as np  
-import matplotlib.pyplot as plt  
-from sklearn.model_selection import train_test_split  
-from sklearn.linear_model import LinearRegression  
-from sklearn import linear_model
-from sklearn import metrics  
+import numpy as np   
 import pickle
 
 #importing model
